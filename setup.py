a2e2530b32b532d4fc1b3a9153917fb738ccf3e9
allfro <ndouba@gmail.com>
2012-07-20 08:31:12 -0400

    First release

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
+from os import path
 
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
 
 setup(
     name='pymiproxy',
@@ -8,6 +11,8 @@ setup(
     author_email='ndouba@gmail.com',
     description='Micro Interceptor Proxy - a simple MITM HTTP/S proxy',
     license='GPL',
+    url='https://github.com/allfro/pymiproxy/',
+    long_description=read('README.md'),
     packages=find_packages('src'),
     package_dir={ '' : 'src' },
     install_requires = [
