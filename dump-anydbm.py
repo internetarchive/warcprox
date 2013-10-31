534c61a4c18f37ff677e3eea7a76cc71d3d7e9d6
Noah Levitt <nlevitt@archive.org>
2013-10-30 17:54:47 -0700

    utility for inspecting deduplication database (or any dbm database)

---
+++ dump-anydbm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# vim:set sw=4 et:
+#
+
+"""
+Dump contents of database to stdout. Database can be any file that the anydbm
+module can read. Included with warcprox because it's useful for inspecting a
+deduplication database, but it is a generic tool.
+"""
+
+import anydbm
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write("usage: {} DBM_FILE\n".format(sys.argv[0]))
+        exit(1)
+
+    db = anydbm.open(sys.argv[1])
+    for key in db:
+        print("{}:{}".format(key, db[key]))
+
