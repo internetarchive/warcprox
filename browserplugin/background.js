51a89a2c5ec2e9a9459dfe24139f7142258daae0
Kelsey Hawley <k.karin.hawley@gmail.com>
2014-01-07 15:51:19 -0800

    simplified some of the js in popup, but then scrapped the idea of a popup interface for a cleaner toggle of the icon.  Now toggles wayback off and on.  However, this does not include anything related to warcprox.  As such, folder moved to new repo. This code will now be altered to work for warcprox settings & requirements

---
+++ browserplugin/background.js
@@ -0,0 +1,43 @@
+/*
+ * Google Chrome Extension built for Archive-It
+ * Extension toggles between turning on & using proxy at specified url and toggling back to normal web browsing.
+ *
+ */
+
+var pac_url = "http://wayback.archive-it.org/proxy.pac";
+var proxy_config = {
+  mode: "pac_script",
+  pacScript: {
+    url: pac_url
+  }
+};
+
+function check_state_and_toggle() {
+    chrome.proxy.settings.get( {}, 
+        function(config) {
+
+            if ( config.value.mode == "pac_script"){
+                // toggle proxy off
+                chrome.proxy.settings.clear({'scope': 'regular'});
+                chrome.browserAction.setBadgeText({ "text" : "OFF"});
+                chrome.browserAction.setBadgeBackgroundColor({ "color" : "#7D0001" });
+                chrome.browserAction.setTitle({"title": "Archive-it Wayback Proxy \nClick to turn proxy ON."});
+            } else {
+                // toggle proxy on
+                chrome.proxy.settings.set(
+                    {value: proxy_config, scope: 'regular'},
+                    function() {});
+                chrome.browserAction.setBadgeText({ "text" : "ON"});  
+                chrome.browserAction.setBadgeBackgroundColor({ "color" : "#14552C" });
+                chrome.browserAction.setTitle({"title": "Archive-it Wayback Proxy\nClick to turn proxy OFF.\nCurrently using proxy: \n" + pac_url});
+            }
+        });
+}
+
+
+chrome.browserAction.onClicked.addListener( check_state_and_toggle );
+chrome.management.onDisabled.addListener( function(ext_info) {
+    // turn proxy settings off when extension disabled/turned off
+    console.log("disabling?");
+    chrome.proxy.settings.clear({'scope': 'regular'});
+});
--- browserplugin/popup.js
+++ browserplugin/popup.js
@@ -1,57 +1,90 @@
+
+var button_text_on = "On";
+var button_text_off = "Off";
+var on_text = "Proxy is currently ON";    
+var off_text = "Proxy is currently OFF";
+var text_id = "toggletext";
+var button_id = "toggle_button";
+
 var proxy_config = {
   mode: "pac_script",
   pacScript: {
     url: "http://wayback.archive-it.org/proxy.pac"
   }
 };
-// chrome.proxy.settings.set(
-//     {value: proxy_config, scope: 'regular'},
-//     function() {});
 
-// chrome.proxy.settings.clear({'scope': 'regular'});
+function toggle_proxy(mode) {
+    if ( mode == "pac_script"){
+        // toggle proxy off
+        chrome.proxy.settings.clear({'scope': 'regular'});
+        update_page("");
 
-// chrome.proxy.settings.get("value.mode") ??
-
-
-on_text = "Proxy is ON";    
-off_text = "Proxy is OFF";
-
-function toggle_proxy(){
-    var button = document.getElementById("toggle_button").firstChild;
-    if( button.data == "On" ){
-        print_to_screen(on_text);
+    } else {
+        // toggle proxy on
         chrome.proxy.settings.set(
             {value: proxy_config, scope: 'regular'},
             function() {});
-    } else {
-        chrome.proxy.settings.clear({'scope': 'regular'});
-        print_to_screen(off_text);
+        update_page("pac_script");
     }
-    button.data = button.data == "On" ? "Off" : "On";
 
+    // debugging only
+    // chrome.proxy.settings.get( {}, 
+    //     function(config) {console.log(JSON.stringify(config.value.mode));});
 }
 
-function print_to_screen(text){
-    toggle_el = document.getElementById("toggletext");
-    // get current state
-    
-    if (toggle_el){ // rewrite text inside
-        var p=document.createElement("P");
-        var t=document.createTextNode(text);
-        p.setAttribute("id", "toggletext");
-        p.appendChild(t); 
-        document.body.replaceChild(p, toggle_el);
+function check_state(func) {
+    if (func) {
+    chrome.proxy.settings.get( {}, 
+        function(config) {
+            console.log(JSON.stringify(config.value.mode));
+            func(config.value.mode);
+        });
+    } else {
+        chrome.proxy.settings.get( {}, 
+        function(config) {
+            console.log(JSON.stringify(config.value.mode));
+            func(config.value.mode);
+        });
+    }
+}
 
-    } else { // create element
-        var p=document.createElement("P");
-        var t=document.createTextNode(text);
-        p.setAttribute("id", "toggletext");
-        p.appendChild(t); 
+function check_state_and_toggle() {
+    check_state(toggle_proxy);
+}
+function check_state_and_update_page() {
+    check_state(update_page);
+}
+function update_page(mode) {
+    if (mode == "pac_script"){
+        // change button to say "Off" and say that proxy is currently ON
+        document.getElementById(button_id).firstChild.data = button_text_off;
+        print_to_screen(on_text);
+        chrome.browserAction.setBadgeText({ "text" : "ON"});
+    } else {
+        // change button to say "On" and say that proxy is currently OFF
+        document.getElementById(button_id).firstChild.data = button_text_on;
+        print_to_screen(off_text);
+        chrome.browserAction.setBadgeText({ "text" : "OFF"});
+    }
+}
+
+function print_to_screen(text) {
+    var p=document.createElement("P");
+    var t=document.createTextNode(text);
+    p.setAttribute("id", text_id);
+    p.appendChild(t); 
+
+    toggle_el = document.getElementById(text_id);
+
+    if (toggle_el) { // if exists, rewrite text inside
+        document.body.replaceChild(p, toggle_el);
+    } else { // else create element
         document.body.appendChild(p);
     }
 }
 
 document.addEventListener('DOMContentLoaded', 
     function() { 
-        document.getElementById("toggle_button").addEventListener("click", toggle_proxy); 
+        check_state_and_update_page();
+        document.getElementById(button_id).addEventListener("click", check_state_and_toggle); 
     });
