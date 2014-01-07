b3edc11139b94125e6627816a621e2a38e5eff0d
Kelsey Hawley <k.karin.hawley@gmail.com>
2014-01-07 12:11:44 -0800

    cleaned up a lot of code and fixed some bad logic in the javascript.  code is a working prototype, but has the bug that, after the 'popup' goes away, the html resets to base state, regardless of proxy state.

--- browserplugin/popup.js
+++ browserplugin/popup.js
@@ -1,9 +1,3 @@
-
-var def_config = {
-  mode: "system"
-}
-
-
 var proxy_config = {
   mode: "pac_script",
   pacScript: {
@@ -14,6 +8,7 @@ var proxy_config = {
 //     {value: proxy_config, scope: 'regular'},
 //     function() {});
 
+// chrome.proxy.settings.clear({'scope': 'regular'});
 
 // chrome.proxy.settings.get("value.mode") ??
 
@@ -21,23 +16,22 @@ var proxy_config = {
 on_text = "Proxy is ON";    
 off_text = "Proxy is OFF";
 
-function onload(){
-    var on_button = document.getElementById("proxy_on");
-    var off_button = document.getElementById("proxy_off");
-    on_button.addEventListener("click", toggle_on);
-    off_button.addEventListener("click", toggle_off)
+function toggle_proxy(){
+    var button = document.getElementById("toggle_button").firstChild;
+    if( button.data == "On" ){
+        print_to_screen(on_text);
+        chrome.proxy.settings.set(
+            {value: proxy_config, scope: 'regular'},
+            function() {});
+    } else {
+        chrome.proxy.settings.clear({'scope': 'regular'});
+        print_to_screen(off_text);
+    }
+    button.data = button.data == "On" ? "Off" : "On";
 
 }
 
-function toggle_on(){
-    toggle(on_text);
-}
-function toggle_off(){
-    toggle(off_text);
-}
-
-function toggle(text){
-
+function print_to_screen(text){
     toggle_el = document.getElementById("toggletext");
     // get current state
     
@@ -55,22 +49,9 @@ function toggle(text){
         p.appendChild(t); 
         document.body.appendChild(p);
     }
-    
-    hide_show_proxy_button();
 }
 
-function hide_show_proxy_button(){
-    on = document.getElementById("proxy_on");
-    off = document.getElementById("proxy_off");
-    
-    if (off.style.display == "none"){
-        off.style.display = "inline";
-        on.style.display = "none";
-    } else {
-        on.style.display = "inline";
-        off.style.display = "none";
-    }
-}
-
-
-onload();
+document.addEventListener('DOMContentLoaded', 
+    function() { 
+        document.getElementById("toggle_button").addEventListener("click", toggle_proxy); 
+    });
