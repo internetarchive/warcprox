a2e3a81028318d5a67feccf742ac127a7463436d
Kelsey Hawley <k.karin.hawley@gmail.com>
2014-01-08 19:02:14 -0800

    working prototype for having the page display a warning, for any new page, when proxy is activated through extension.  also removed a lot of unneeded code from previous plugin incarnation

--- browserplugin/background.js
+++ browserplugin/background.js
@@ -4,35 +4,23 @@
  *
  */
 
-var pac_url = "http://wayback.archive-it.org/proxy.pac";
-var proxy_config = {
-  mode: "pac_script",
-  pacScript: {
-    url: pac_url
-  }
-};
 
-function check_state_and_toggle() {
-    chrome.proxy.settings.get( {}, 
-        function(config) {
+var proxy_used = "system";
 
-            if ( config.value.mode == "pac_script"){
-                // toggle proxy off
-                chrome.proxy.settings.clear({'scope': 'regular'});
-                chrome.browserAction.setBadgeText({ "text" : "OFF"});
-                chrome.browserAction.setBadgeBackgroundColor({ "color" : "#7D0001" });
-                chrome.browserAction.setTitle({"title": "Archive-it Wayback Proxy \nClick to turn proxy ON."});
-            } else {
-                // toggle proxy on
-                chrome.proxy.settings.set(
-                    {value: proxy_config, scope: 'regular'},
-                    function() {});
-                chrome.browserAction.setBadgeText({ "text" : "ON"});  
-                chrome.browserAction.setBadgeBackgroundColor({ "color" : "#14552C" });
-                chrome.browserAction.setTitle({"title": "Archive-it Wayback Proxy\nClick to turn proxy OFF.\nCurrently using proxy: \n" + pac_url});
-            }
-        });
-}
-
-
-chrome.browserAction.onClicked.addListener( check_state_and_toggle );
+chrome.tabs.onUpdated.addListener(
+    function(id, changeInfo, updatedTab) {
+        console.log("tab "+id+" status "+ JSON.stringify(changeInfo.status));  
+        chrome.proxy.settings.get( {}, 
+            function(config) {
+                console.log(JSON.stringify(config.value.mode));
+                if(config.value.mode == proxy_used && changeInfo.status == "loading") {
+                    chrome.tabs.executeScript( id, {file: "page.js"}, function (result) {
+                        console.log("-loaded page.js");
+                    });
+                    chrome.tabs.insertCSS( id, {file: "page.css"}, function (result) {
+                        console.log("-loaded page.css");
+                    });
+                }
+            });
+  
+    });
--- browserplugin/popup.js
+++ browserplugin/popup.js
@@ -1,90 +1,36 @@
 
-var button_text_on = "On";
-var button_text_off = "Off";
-var on_text = "Proxy is currently ON";    
-var off_text = "Proxy is currently OFF";
-var text_id = "toggletext";
-var button_id = "toggle_button";
+function startWarcprox() {
+    var given_host = "127.0.0.1";//document.getElementById("host");
+    var given_port = 8000;//document.getElementById("port");
 
-var proxy_config = {
-  mode: "pac_script",
-  pacScript: {
-    url: "http://wayback.archive-it.org/proxy.pac"
-  }
-};
+    var proxy_config = {
+        mode: "fixed_servers",
+        rules: {
+            proxyForHttp: {
+                scheme: "http",
+                host: given_host,
+                port: given_port
+            },
+            // bypassList: ["foobar.com"]
+        }
+    };
+    // chrome.proxy.settings.set(
+    //         {value: proxy_config, scope: 'regular'},
+    //         function() {});
+    chrome.browserAction.setBadgeText({ "text" : "ON"});
 
-function toggle_proxy(mode) {
-    if ( mode == "pac_script"){
-        // toggle proxy off
-        chrome.proxy.settings.clear({'scope': 'regular'});
-        update_page("");
-
-    } else {
-        // toggle proxy on
-        chrome.proxy.settings.set(
-            {value: proxy_config, scope: 'regular'},
-            function() {});
-        update_page("pac_script");
-    }
-
-    // debugging only
-    // chrome.proxy.settings.get( {}, 
-    //     function(config) {console.log(JSON.stringify(config.value.mode));});
 }
 
-function check_state(func) {
-    if (func) {
-    chrome.proxy.settings.get( {}, 
-        function(config) {
-            console.log(JSON.stringify(config.value.mode));
-            func(config.value.mode);
-        });
-    } else {
-        chrome.proxy.settings.get( {}, 
-        function(config) {
-            console.log(JSON.stringify(config.value.mode));
-            func(config.value.mode);
-        });
-    }
+function stopWarcprox() {
+    chrome.proxy.settings.clear({'scope': 'regular'});
+    chrome.browserAction.setBadgeText({ "text" : ""});
 }
 
-function check_state_and_toggle() {
-    check_state(toggle_proxy);
-}
-function check_state_and_update_page() {
-    check_state(update_page);
-}
-function update_page(mode) {
-    if (mode == "pac_script"){
-        // change button to say "Off" and say that proxy is currently ON
-        document.getElementById(button_id).firstChild.data = button_text_off;
-        print_to_screen(on_text);
-        chrome.browserAction.setBadgeText({ "text" : "ON"});
-    } else {
-        // change button to say "On" and say that proxy is currently OFF
-        document.getElementById(button_id).firstChild.data = button_text_on;
-        print_to_screen(off_text);
-        chrome.browserAction.setBadgeText({ "text" : "OFF"});
-    }
-}
-
-function print_to_screen(text) {
-    var p=document.createElement("P");
-    var t=document.createTextNode(text);
-    p.setAttribute("id", text_id);
-    p.appendChild(t); 
-
-    toggle_el = document.getElementById(text_id);
-
-    if (toggle_el) { // if exists, rewrite text inside
-        document.body.replaceChild(p, toggle_el);
-    } else { // else create element
-        document.body.appendChild(p);
-    }
-}
 
 document.addEventListener('DOMContentLoaded', 
     function() { 
-        check_state_and_update_page();
-        document.getElementById(button_id).addEventListener("click", check_state_and_toggle); 
+        // check_state_and_update_page();
+        chrome.proxy.settings.clear({'scope': 'regular'});
+        document.getElementById("submit_settings").addEventListener("click", startWarprox); 
+
     });
