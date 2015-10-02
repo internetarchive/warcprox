e1474133b638e1f442b812ee794a79b6038f6a69
Noah Levitt <nlevitt@archive.org>
2015-10-02 16:55:11 +0000

    does it still hang if we don't really run anything inside docker?

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -9,11 +9,12 @@ user=$(id -un)
 
 for python in python2.7 python3.4
 do
-	docker run --net=host --rm --volume="$script_dir/..:/rethinkstuff" internetarchive/rethinkdb /sbin/my_init -- \
-		bash -x -c "cd /rethinkstuff \
-				&& virtualenv -p $python /tmp/venv \
-				&& source /tmp/venv/bin/activate \
-				&& pip install pytest . \
-				&& py.test -v -s tests"
+	docker run --rm --volume="$script_dir/..:/rethinkstuff" internetarchive/rethinkdb /sbin/my_init -- \
+		true
+		# bash -x -c "cd /rethinkstuff \
+		# 		&& virtualenv -p $python /tmp/venv \
+		# 		&& source /tmp/venv/bin/activate \
+		# 		&& pip install pytest . \
+		# 		&& py.test -v -s tests"
 done
 
