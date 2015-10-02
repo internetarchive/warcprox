7e96d65f960b1d0dd4fe047e88ef212242b85120
Noah Levitt <nlevitt@archive.org>
2015-10-02 01:28:29 +0000

    ugh wait that long for travis to find this mistake

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -9,12 +9,11 @@ user=$(id -un)
 
 for python in python2.7 python3.4
 do
-	docker run --rm -i -t --volume="$script_dir/..:/rethinkstuff" internetarchive/rethinkdb /sbin/my_init -- \
-		bash -x -c "adduser --gecos=$user --disabled-password --quiet --uid=$uid $user \
-			&& sudo -u $user bash -x -c 'cd /rethinkstuff \
+	docker run --net=host --rm -i -t --volume="$script_dir/..:/rethinkstuff" internetarchive/rethinkdb /sbin/my_init -- \
+		bash -x -c "cd /rethinkstuff \
 				&& virtualenv -p $python /tmp/venv \
 				&& source /tmp/venv/bin/activate \
 				&& pip install pytest . \
-				&& py.test -v -s tests'"
+				&& py.test -v -s tests"
 done
 
