03c506dade2ad3ca145c91b3562ae3d885a1984c
Noah Levitt <nlevitt@archive.org>
2015-10-28 21:35:49 +0000

    stop after first failing test, use py.test -s

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,6 +7,8 @@ docker build -t internetarchive/rethinkdb $script_dir || exit 1
 uid=$(id -u)
 user=$(id -un)
 
+set -e
+
 for python in python2.7 python3.4
 do
 	docker run --rm -i -t --volume="$script_dir/..:/warcprox" internetarchive/rethinkdb /sbin/my_init -- \
@@ -16,8 +18,8 @@ do
 				&& virtualenv -p $python /tmp/venv \
 				&& source /tmp/venv/bin/activate \
 				&& pip --log-file /tmp/pip.log install . pytest requests \
-				&& py.test tests \
-				&& py.test --rethinkdb-servers=localhost tests \
-				&& py.test --rethinkdb-servers=localhost --rethinkdb-big-table tests'"
+				&& py.test -s tests \
+				&& py.test -s --rethinkdb-servers=localhost tests \
+				&& py.test -s --rethinkdb-servers=localhost --rethinkdb-big-table tests'"
 done
 
