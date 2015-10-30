58a7d48cd53cfdcb75506708cd345a2f8531b920
Noah Levitt <nlevitt@archive.org>
2015-10-30 19:51:50 +0000

    version number 0.1

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='rethinkstuff',
+    version='0.1',
     packages=['rethinkstuff'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     install_requires=['rethinkdb'],
 )
