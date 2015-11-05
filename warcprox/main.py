a9fc550453ac51edb2f6e333fb538bbac0c8dee2
Noah Levitt <nlevitt@archive.org>
2015-11-05 02:26:43 +0000

    oops, argparse.SUPPRESS isn't supposed to be in quotes

--- warcprox/main.py
+++ warcprox/main.py
@@ -79,7 +79,7 @@ def _build_arg_parser(prog=os.path.basename(sys.argv[0])):
     arg_parser.add_argument('--kafka-capture-feed-topic', dest='kafka_capture_feed_topic', 
             default=None, help='kafka capture feed topic')
     arg_parser.add_argument('--queue-size', dest='queue_size', default=1000,
-            help='argparse.SUPPRESS')
+            help=argparse.SUPPRESS)
     arg_parser.add_argument('--version', action='version',
             version="warcprox {}".format(warcprox.__version__))
     arg_parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
