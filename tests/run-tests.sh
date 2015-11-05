f38ce708bfbf64e4712c7da2705f5e87988525ac
Noah Levitt <nlevitt@archive.org>
2015-11-05 02:55:52 +0000

    set PYTHONDONTWRITEBYTECODE in one place

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -13,12 +13,12 @@ for python in python2.7 python3.4
 do
 	docker run --rm -i -t --volume="$script_dir/..:/warcprox" internetarchive/rethinkdb /sbin/my_init -- \
 		bash -x -c " adduser --gecos=$user --disabled-password --quiet --uid=$uid $user \
-			&& sudo -u $user bash -x -c 'cd /warcprox \
+			&& sudo PYTHONDONTWRITEBYTECODE=1 -u $user bash -x -c 'cd /warcprox \
 				&& virtualenv -p $python /tmp/venv \
 				&& source /tmp/venv/bin/activate \
 				&& pip --log-file /tmp/pip.log install . pytest requests \
-				&& PYTHONDONTWRITEBYTECODE=1 py.test -s tests \
-				&& PYTHONDONTWRITEBYTECODE=1 py.test -s --rethinkdb-servers=localhost tests \
-				&& PYTHONDONTWRITEBYTECODE=1 py.test -s --rethinkdb-servers=localhost --rethinkdb-big-table tests'"
+				&& py.test -s tests \
+				&& py.test -s --rethinkdb-servers=localhost tests \
+				&& py.test -s --rethinkdb-servers=localhost --rethinkdb-big-table tests'"
 done
 
