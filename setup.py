79b3895da32c9be0fbf8d5b6a871df21f01c558f
Noah Levitt <nlevitt@archive.org>
2015-11-06 23:42:45 +0000

    bump version again for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='rethinkstuff',
-    version='0.1.1',
+    version='0.1.2',
     packages=['rethinkstuff'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
