564a058a9ee87a19afd847db00f0a8b7355fd3d6
Noah Levitt <nlevitt@archive.org>
2016-11-21 15:19:52 -0800

    call this 2.0b2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.0b2.dev39',
+        version='2.0b2',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
