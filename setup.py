c5da679b1d5b8e21a0d9c928996d5a374d917f3b
Noah Levitt <nlevitt@archive.org>
2017-04-18 17:13:54 -0700

    bump version number for pull request #2, and fiddle with .travis.yml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 
 setuptools.setup(
     name='doublethink',
-    version='0.2.0.dev72',
+    version='0.2.0.dev73',
     packages=['doublethink'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
