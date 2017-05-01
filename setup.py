6c57b81de8d4f21234204e0a0e4aa360dd6c83d9
Noah Levitt <nlevitt@archive.org>
2017-05-01 14:01:01 -0700

    recover from a transient error, which happens for me at least rethinkdb starts up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 
 setuptools.setup(
     name='doublethink',
-    version='0.2.0.dev75',
+    version='0.2.0.dev76',
     packages=['doublethink'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
