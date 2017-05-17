def44503bf83a21ef0581fc3b2fa562e416713e3
Noah Levitt <nlevitt@archive.org>
2017-05-17 12:29:55 -0700

    bump version after merging pr #5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 
 setuptools.setup(
     name='doublethink',
-    version='0.2.0.dev79',
+    version='0.2.0.dev80',
     packages=['doublethink'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
