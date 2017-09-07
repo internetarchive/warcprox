3003c46c10de99f134ab3f527d5cc48d400ba569
Noah Levitt <nlevitt@archive.org>
2017-09-07 10:33:21 -0700

    https://github.com/internetarchive/warcprox/pull/32 warrants a version bump

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.1b1.dev96',
+        version='2.2b1.dev97',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
