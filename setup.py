be6fe83c569eccbda32c266a6f8df607973c082f
Noah Levitt <nlevitt@archive.org>
2017-09-28 14:37:30 -0700

    bump dev version number after merging pull requests

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.2b1.dev101',
+        version='2.2b1.dev102',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
