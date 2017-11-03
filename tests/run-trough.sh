ba7497525ae2824920f453a98b20888feafe5e36
Noah Levitt <nlevitt@archive.org>
2017-11-03 14:21:39 -0700

    update travis-ci trough deployment

--- tests/run-trough.sh
+++ tests/run-trough.sh
@@ -4,7 +4,7 @@
 #
 
 pip install git+https://github.com/jkafader/snakebite@feature/python3-version-string
-pip install git+https://github.com/nlevitt/trough.git@9ae5f477a28f22
+pip install git+https://github.com/internetarchive/trough.git@toward-warcprox-dedup
 
 mkdir /etc/trough
 
@@ -26,8 +26,9 @@ rr.db('trough_configuration').wait().run()"
 
 sync.py --server >>/tmp/trough-sync-server.out 2>&1 &
 uwsgi --http :6222 --master --processes=2 --harakiri=240 --max-requests=50000 --vacuum --die-on-term --wsgi-file /usr/local/bin/writer.py >>/tmp/trough-write.out 2>&1 &
-uwsgi --http :6112 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --wsgi-file /usr/local/bin/write_provisioner_local.py >>/tmp/trough-write-provisioner-local.out 2>&1 &
-uwsgi --http :6111 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --wsgi-file /usr/local/bin/write_provisioner_server.py >>/tmp/trough-write-provisioner-server.out 2>&1 &
+uwsgi --http :6112 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --mount /=trough.wsgi.segment_manager:local >>/tmp/trough-segment-manager-local.out 2>&1 &
+uwsgi --http :6111 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --mount /=trough.wsgi.segment_manager:server >>/tmp/trough-segment-manager-server.out 2>&1 &
 uwsgi --http :6444 --master --processes=2 --harakiri=3200 --socket-timeout=3200 --max-requests=50000 --vacuum --die-on-term --wsgi-file /usr/local/bin/reader.py >>/tmp/trough-read.out 2>&1 &
 
 wait
+
