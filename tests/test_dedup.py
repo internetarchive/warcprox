4a165e5f779275793ab720ba000d1463777556aa
Vangelis Banos <vangelis@archive.org>
2018-01-15 20:58:36 +0000

    Update CdxServerDedup unit test
    To work correctly with the new way we init the
    ``CdxServerDedup.http_pool``. Use ``mock.MagicMock`` instead of
    ``mock.patch``. The unit test logic remains entirely the same.

--- tests/test_dedup.py
+++ tests/test_dedup.py
@@ -4,43 +4,43 @@ from warcprox.dedup import CdxServerDedup
 
 def test_cdx_dedup():
     # Mock CDX Server responses to simulate found, not found and errors.
-    with mock.patch('warcprox.dedup.CdxServerDedup.http_pool.request') as request:
-        url = "http://example.com"
-        # not found case
-        result = mock.Mock()
-        result.status = 200
-        result.data = b'20170101020405 test'
-        request.return_value = result
-        cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
-        res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
-                                url=url)
-        assert res is None
+    url = "http://example.com"
+    # not found case
+    result = mock.Mock()
+    result.status = 200
+    result.data = b'20170101020405 test'
+    cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
+    cdx_server.http_pool.request = mock.MagicMock(return_value=result)
+    res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
+                            url=url)
+    assert res is None
 
-        # found case
-        result = mock.Mock()
-        result.status = 200
-        result.data = b'20170203040503 B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A'
-        request.return_value = result
-        cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
-        res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
-                                url=url)
-        assert res["date"] == b"2017-02-03T04:05:03Z"
+    # found case
+    result = mock.Mock()
+    result.status = 200
+    result.data = b'20170203040503 B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A'
+    cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
+    cdx_server.http_pool.request = mock.MagicMock(return_value=result)
+    res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
+                            url=url)
+    assert res["date"] == b"2017-02-03T04:05:03Z"
 
-        # invalid CDX result status code
-        result = mock.Mock()
-        result.status = 400
-        result.data = b'20170101020405 B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A'
-        request.return_value = result
-        cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
-        res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
-                                url=url)
-        assert res is None
-        # invalid CDX result content
-        result = mock.Mock()
-        result.status = 200
-        result.data = b'InvalidExceptionResult'
-        request.return_value = result
-        cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
-        res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
-                                url=url)
-        assert res is None
+    # invalid CDX result status code
+    result = mock.Mock()
+    result.status = 400
+    result.data = b'20170101020405 B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A'
+    cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
+    cdx_server.http_pool.request = mock.MagicMock(return_value=result)
+    res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
+                            url=url)
+    assert res is None
+
+    # invalid CDX result content
+    result = mock.Mock()
+    result.status = 200
+    result.data = b'InvalidExceptionResult'
+    cdx_server = CdxServerDedup(cdx_url="dummy-cdx-server-url")
+    cdx_server.http_pool.request = mock.MagicMock(return_value=result)
+    res = cdx_server.lookup(digest_key="B2LTWWPUOYAH7UIPQ7ZUPQ4VMBSVC36A",
+                            url=url)
+    assert res is None
