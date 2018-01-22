41b531e398798ec9d13e1e2ab81ae9c93e08672a
Noah Levitt <nlevitt@archive.org>
2018-01-21 19:47:15 -0800

    use trick to avoid dns looking up local ip

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.4b1.dev140',
+        version='2.4b1.dev141',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
