2bbe60a4cb82cec9b51e584534e75aeb7213701d
Barbara Miller <galgeek@me.com>
2018-02-20 15:49:58 -0800

    no need for local var

--- warcprox/writerthread.py
+++ warcprox/writerthread.py
@@ -81,11 +81,8 @@ class WarcWriterProcessor(warcprox.BaseStandardPostfetchProcessor):
                   if recorded_url.warcprox_meta
                      and 'warc-prefix' in recorded_url.warcprox_meta
                   else self.options.prefix)
-        do_not_archive = (recorded_url.do_not_archive
-                          if recorded_url.do_not_archive
-                          else False)
         # special warc name prefix '-' means "don't archive"
-        return (prefix != '-' and (not do_not_archive)
+        return (prefix != '-' and not recorded_url.do_not_archive
                 and self._filter_accepts(recorded_url))
 
     def _log(self, recorded_url, records):
