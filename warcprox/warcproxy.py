7c814d71ba3da27dd431848a82c8ae19408cc472
Noah Levitt <nlevitt@archive.org>
2018-04-04 13:26:47 -0700

    close all remote connections at shutdown
    to avoid hang

--- warcprox/warcproxy.py
+++ warcprox/warcproxy.py
@@ -483,6 +483,7 @@ class WarcProxy(SingleThreadedWarcProxy, warcprox.mitmproxy.PooledMitmProxy):
     def server_close(self):
         self.logger.info('shutting down')
         http_server.HTTPServer.server_close(self)
+        self.remote_connection_pool.clear()
 
     def handle_error(self, request, client_address):
         self.logger.warn(
