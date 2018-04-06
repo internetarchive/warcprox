ebf5453c2fcf0908dc304ab310acf180a5ea5749
Noah Levitt <nlevitt@archive.org>
2018-04-06 13:26:56 -0700

    bump dev version number after PR

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.4b2.dev163',
+        version='2.4b2.dev164',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
