a43fb74464087e2abb52a1587e31b0e171f26ce6
Noah Levitt <nlevitt@archive.org>
2018-09-27 13:40:01 -0700

    bump version after merge

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setuptools.setup(
     name='doublethink',
-    version='0.2.0.dev89',
+    version='0.2.0.dev90',
     packages=['doublethink'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
