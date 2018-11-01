72d6e4d39b19d9b73e8f4bad2c719c14742808c2
Noah Levitt <nlevitt@archive.org>
2018-11-01 19:23:28 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setuptools.setup(
     name='doublethink',
-    version='0.2.0.dev92',
+    version='0.2.0.dev93',
     packages=['doublethink'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
