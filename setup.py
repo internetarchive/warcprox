240d95f7cede6826b63463a27be52f627e1d5ec1
Christian Clauss <cclauss@me.com>
2023-06-13 10:48:11 +0200

    PEP 440: setuptools.setup(version='2.4.32.dev0')
    https://peps.python.org/pep-0440/

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.4.32.qa',
+        version='2.4.32.dev0',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
