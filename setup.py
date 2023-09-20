bc0da12c484fef7c099eaa216d92b6ff8acc084a
Barbara Miller <galgeek@users.noreply.github.com>
2023-09-20 10:57:54 -0700

    bump version for Py311

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.4.31',
+        version='2.4.32',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
