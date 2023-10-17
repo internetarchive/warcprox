af52dec4694c1903c10ed0e0cdce62626fd4cf0d
Barbara Miller <3253863+galgeek@users.noreply.github.com>
2023-10-17 09:19:56 -0700

    bump version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.5',
+        version='2.5.1',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
