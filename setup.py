2ba1301376fed7a5b6e1e166e9b8eaafba708bc4
Misty De Méo <misty@archive.org>
2025-07-21 14:48:03 -0700

    release: 2.8.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ except:
 
 setuptools.setup(
         name='warcprox',
-        version='2.8.0',
+        version='2.8.1',
         description='WARC writing MITM HTTP/S proxy',
         url='https://github.com/internetarchive/warcprox',
         author='Noah Levitt',
